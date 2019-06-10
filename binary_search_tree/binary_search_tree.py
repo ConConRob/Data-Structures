bb4f0ae0a8be2d80073bc869b3f482eb2d03dccc
conconrob <connortprobertson@gmail.com>
2019-06-10 15:22:36 +0100

    bi search tree insert done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,34 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # check if higher
+        if value > self.value:
+            # check if right
+            if self.right:
+                # insert into right tree
+                self.right.insert(value)
+            # create new tree and set as right
+            else:
+                self.right = BinarySearchTree(value)
+        # if lower
+        elif value < self.value:
+            # check if left
+            if self.left:
+                # insert into left tree
+                self.left.insert(value)
+            # create new tree and set as left
+            else:
+                self.left = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def for_each(self, cb):
-    pass
+    def for_each(self, cb):
+        pass
